d4a92e17f091014afb6f1c46233260b6d47f14b2
jeremyfa <contact@jeremyfa.com>
2017-06-09 18:34:01 +0200

    In debug builds, provide better unsafeGet/Set errors

--- src/ceramic/Extensions.hx
+++ src/ceramic/Extensions.hx
@@ -6,6 +6,9 @@ class Extensions {
 /// Array extensions
 
     inline public static function unsafeGet<T>(array:Array<T>, index:Int):T {
+#if debug
+        if (index < 0 || index >= array.length) throw 'Invalid unsafeGet: index=$index length=${array.length}';
+#end
 #if cpp
         return cpp.NativeArray.unsafeGet(array, index);
 #else
@@ -14,6 +17,9 @@ class Extensions {
     } //unsafeGet
 
     inline public static function unsafeSet<T>(array:Array<T>, index:Int, value:T):Void {
+#if debug
+        if (index < 0 || index >= array.length) throw 'Invalid unsafeSet: index=$index length=${array.length}';
+#end
 #if cpp
         cpp.NativeArray.unsafeSet(array, index, value);
 #else
