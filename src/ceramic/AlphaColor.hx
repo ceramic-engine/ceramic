05e796801d82fb01e5286ff253cbe2ebff5e8561
jeremyfa <contact@jeremyfa.com>
2017-06-09 19:09:29 +0200

    Tidy: remove tabs that sneaked-in

--- src/ceramic/AlphaColor.hx
+++ src/ceramic/AlphaColor.hx
@@ -5,15 +5,15 @@ package ceramic;
     constructed from Color/Int (RGB) + Float (A). */
 abstract AlphaColor(Int) from Int from UInt to Int to UInt {
 
-	public var red(get, set):Int;
-	public var blue(get, set):Int;
-	public var green(get, set):Int;
-	public var alpha(get, set):Int;
-	
-	public var redFloat(get, set):Float;
-	public var blueFloat(get, set):Float;
-	public var greenFloat(get, set):Float;
-	public var alphaFloat(get, set):Float;
+    public var red(get, set):Int;
+    public var blue(get, set):Int;
+    public var green(get, set):Int;
+    public var alpha(get, set):Int;
+    
+    public var redFloat(get, set):Float;
+    public var blueFloat(get, set):Float;
+    public var greenFloat(get, set):Float;
+    public var alphaFloat(get, set):Float;
 
     public var color(get, set):Color;
 
@@ -32,98 +32,98 @@ abstract AlphaColor(Int) from Int from UInt to Int to UInt {
         blue = color.blue;
         return color;
     }
-	
-	private inline function get_red():Int
-	{
-		return (this >> 16) & 0xff;
-	}
-	
-	private inline function get_green():Int
-	{
-		return (this >> 8) & 0xff;
-	}
-	
-	private inline function get_blue():Int
-	{
-		return this & 0xff;
-	}
-	
-	private inline function get_alpha():Int
-	{
-		return (this >> 24) & 0xff;
-	}
-	
-	private inline function get_redFloat():Float
-	{
-		return red / 255;
-	}
-	
-	private inline function get_greenFloat():Float
-	{
-		return green / 255;
-	}
-	
-	private inline function get_blueFloat():Float
-	{
-		return blue / 255;
-	}
-	
-	private inline function get_alphaFloat():Float
-	{
-		return alpha / 255;
-	}
-	
-	private inline function set_red(Value:Int):Int
-	{
-		this &= 0xff00ffff;
-		this |= boundChannel(Value) << 16;
-		return Value;
-	}
-	
-	private inline function set_green(Value:Int):Int
-	{
-		this &= 0xffff00ff;
-		this |= boundChannel(Value) << 8;
-		return Value;
-	}
-	
-	private inline function set_blue(Value:Int):Int
-	{
-		this &= 0xffffff00;
-		this |= boundChannel(Value);
-		return Value;
-	}
-	
-	private inline function set_alpha(Value:Int):Int
-	{
-		this &= 0x00ffffff;
-		this |= boundChannel(Value) << 24;
-		return Value;
-	}
-	
-	private inline function set_redFloat(Value:Float):Float
-	{
-		red = Math.round(Value * 255);
-		return Value;
-	}
-	
-	private inline function set_greenFloat(Value:Float):Float
-	{
-		green = Math.round(Value * 255);
-		return Value;
-	}
-	
-	private inline function set_blueFloat(Value:Float):Float
-	{
-		blue = Math.round(Value * 255);
-		return Value;
-	}
-	
-	private inline function set_alphaFloat(Value:Float):Float
-	{
-		alpha = Math.round(Value * 255);
-		return Value;
-	}
+    
+    private inline function get_red():Int
+    {
+        return (this >> 16) & 0xff;
+    }
+    
+    private inline function get_green():Int
+    {
+        return (this >> 8) & 0xff;
+    }
+    
+    private inline function get_blue():Int
+    {
+        return this & 0xff;
+    }
+    
+    private inline function get_alpha():Int
+    {
+        return (this >> 24) & 0xff;
+    }
+    
+    private inline function get_redFloat():Float
+    {
+        return red / 255;
+    }
+    
+    private inline function get_greenFloat():Float
+    {
+        return green / 255;
+    }
+    
+    private inline function get_blueFloat():Float
+    {
+        return blue / 255;
+    }
+    
+    private inline function get_alphaFloat():Float
+    {
+        return alpha / 255;
+    }
+    
+    private inline function set_red(Value:Int):Int
+    {
+        this &= 0xff00ffff;
+        this |= boundChannel(Value) << 16;
+        return Value;
+    }
+    
+    private inline function set_green(Value:Int):Int
+    {
+        this &= 0xffff00ff;
+        this |= boundChannel(Value) << 8;
+        return Value;
+    }
+    
+    private inline function set_blue(Value:Int):Int
+    {
+        this &= 0xffffff00;
+        this |= boundChannel(Value);
+        return Value;
+    }
+    
+    private inline function set_alpha(Value:Int):Int
+    {
+        this &= 0x00ffffff;
+        this |= boundChannel(Value) << 24;
+        return Value;
+    }
+    
+    private inline function set_redFloat(Value:Float):Float
+    {
+        red = Math.round(Value * 255);
+        return Value;
+    }
+    
+    private inline function set_greenFloat(Value:Float):Float
+    {
+        green = Math.round(Value * 255);
+        return Value;
+    }
+    
+    private inline function set_blueFloat(Value:Float):Float
+    {
+        blue = Math.round(Value * 255);
+        return Value;
+    }
+    
+    private inline function set_alphaFloat(Value:Float):Float
+    {
+        alpha = Math.round(Value * 255);
+        return Value;
+    }
 
     private inline function boundChannel(value:Int):Int
     {
