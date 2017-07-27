e4bc4c04ddf1abdaa7bcd1a1f65b717a405f6864
jeremyfa <contact@jeremyfa.com>
2017-07-27 15:40:36 +0200

    Fix unresolved libneko errors.

--- ceramic-env.js
+++ ceramic-env.js
@@ -13,10 +13,15 @@ if (process.platform == 'darwin') {
 
     // Expose Neko
     var nekoPath = path.join(vendorDir, 'neko');
-    if (!fs.existsSync('/usr/local/lib/neko')) {
-        // Link with local neko, only if not existing already
-        spawnSync('ln', ['-s', nekoPath, 'neko'], { cwd: '/usr/local/lib' });
+    // Link with local neko, only if not existing already
+    if (!fs.existsSync('/usr/local/lib/neko')) spawnSync('ln', ['-s', nekoPath, 'neko'], { cwd: '/usr/local/lib' });
+    if (!fs.existsSync('/usr/local/lib/libneko.dylib')) {
+        spawnSync('ln', ['-s', '/usr/local/lib/neko/libneko.dylib', 'libneko.dylib'], { cwd: '/usr/local/lib' });
+        spawnSync('ln', ['-s', '/usr/local/lib/neko/libneko.2.1.0.dylib', 'libneko.2.1.0.dylib'], { cwd: '/usr/local/lib' });
+        spawnSync('ln', ['-s', '/usr/local/lib/neko/libneko.2.dylib', 'libneko.2.dylib'], { cwd: '/usr/local/lib' });
     }
+    if (!fs.existsSync('/usr/local/lib/nekoc')) spawnSync('ln', ['-s', '/usr/local/lib/neko/nekoc', 'nekoc'], { cwd: '/usr/local/lib' });
+    if (!fs.existsSync('/usr/local/lib/nekotools')) spawnSync('ln', ['-s', '/usr/local/lib/neko/nekotools', 'nekoc'], { cwd: '/usr/local/lib' });
     process.env['PATH'] = nekoPath + ':' + process.env['PATH'];
     process.env['DYLD_LIBRARY_PATH'] = process.env['DYLD_LIBRARY_PATH'] != null ?
         nekoPath + ':' + process.env['DYLD_LIBRARY_PATH'] :
