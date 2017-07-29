5187788d3adc21ef25c7cda5003ca38a9116717e
jeremyfa <contact@jeremyfa.com>
2017-07-30 00:22:09 +0200

    Make ceramic-env (neko/haxe on mac) more robust.

--- ceramic-env.js
+++ ceramic-env.js
@@ -15,22 +15,45 @@ if (process.platform == 'darwin') {
     //
     var haxePath = path.join(vendorDir, 'haxe');
     if (!fs.existsSync('/usr/local/bin/haxe')) {
+        spawnSync('rm', ['haxe'], { cwd: '/usr/local/bin' });
         spawnSync('ln', ['-s', path.join(haxePath, 'haxe'), 'haxe'], { cwd: '/usr/local/bin', stdio: 'inherit' });
     }
-    if (!fs.existsSync('/usr/local/bin/haxelib')) spawnSync('ln', ['-s', path.join(haxePath, 'haxelib'), 'haxelib'], { cwd: '/usr/local/bin' });
-    if (!fs.existsSync('/usr/local/lib/haxe')) spawnSync('ln', ['-s', haxePath, 'haxe'], { cwd: '/usr/local/lib' });
+    if (!fs.existsSync('/usr/local/bin/haxelib')) {
+        spawnSync('rm', ['haxelib'], { cwd: '/usr/local/bin' });
+        spawnSync('ln', ['-s', path.join(haxePath, 'haxelib'), 'haxelib'], { cwd: '/usr/local/bin' });
+    }
+    if (!fs.existsSync('/usr/local/lib/haxe')) {
+        spawnSync('rm', ['haxe'], { cwd: '/usr/local/lib' });
+        spawnSync('ln', ['-s', haxePath, 'haxe'], { cwd: '/usr/local/lib' });
+    }
 
     // Expose Neko
     //
     var nekoPath = path.join(vendorDir, 'neko');
-    if (!fs.existsSync('/usr/local/lib/neko')) spawnSync('ln', ['-s', nekoPath, 'neko'], { cwd: '/usr/local/lib' });
+    if (!fs.existsSync('/usr/local/bin/neko')) {
+        spawnSync('rm', ['neko'], { cwd: '/usr/local/bin' });
+        spawnSync('ln', ['-s', path.join(nekoPath, 'neko'), 'neko'], { cwd: '/usr/local/bin' });
+    }
+    if (!fs.existsSync('/usr/local/lib/neko')) {
+        spawnSync('rm', ['neko'], { cwd: '/usr/local/lib' });
+        spawnSync('ln', ['-s', nekoPath, 'neko'], { cwd: '/usr/local/lib' });
+    }
     if (!fs.existsSync('/usr/local/lib/libneko.dylib')) {
+        spawnSync('rm', ['libneko.dylib'], { cwd: '/usr/local/lib' });
+        spawnSync('rm', ['libneko.2.1.0.dylib'], { cwd: '/usr/local/lib' });
+        spawnSync('rm', ['libneko.2.dylib'], { cwd: '/usr/local/lib' });
         spawnSync('ln', ['-s', '/usr/local/lib/neko/libneko.dylib', 'libneko.dylib'], { cwd: '/usr/local/lib' });
         spawnSync('ln', ['-s', '/usr/local/lib/neko/libneko.2.1.0.dylib', 'libneko.2.1.0.dylib'], { cwd: '/usr/local/lib' });
         spawnSync('ln', ['-s', '/usr/local/lib/neko/libneko.2.dylib', 'libneko.2.dylib'], { cwd: '/usr/local/lib' });
     }
-    if (!fs.existsSync('/usr/local/lib/nekoc')) spawnSync('ln', ['-s', '/usr/local/lib/neko/nekoc', 'nekoc'], { cwd: '/usr/local/lib' });
-    if (!fs.existsSync('/usr/local/lib/nekotools')) spawnSync('ln', ['-s', '/usr/local/lib/neko/nekotools', 'nekotools'], { cwd: '/usr/local/lib' });
+    if (!fs.existsSync('/usr/local/lib/nekoc')) {
+        spawnSync('rm', ['nekoc'], { cwd: '/usr/local/lib' });
+        spawnSync('ln', ['-s', '/usr/local/lib/neko/nekoc', 'nekoc'], { cwd: '/usr/local/lib' });
+    }
+    if (!fs.existsSync('/usr/local/lib/nekotools')) {
+        spawnSync('rm', ['nekotools'], { cwd: '/usr/local/lib' });
+        spawnSync('ln', ['-s', '/usr/local/lib/neko/nekotools', 'nekotools'], { cwd: '/usr/local/lib' });
+    }
 
 }
 
