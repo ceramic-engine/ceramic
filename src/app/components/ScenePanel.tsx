263aa868def6f0505b197060b1ac0971cad492ea
jeremyfa <contact@jeremyfa.com>
2017-08-06 19:07:29 +0200

    Make scene properties panel a bit nicer.

--- src/app/components/ScenePanel.tsx
+++ src/app/components/ScenePanel.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { observer } from 'utils';
-import { Button, Form, Field, Panel, NumberInput } from 'components';
+import { Button, Form, Field, Panel, NumberInput, Title, Alt } from 'components';
 import { project } from 'app/model';
 
 @observer class ScenePanel extends React.Component {
@@ -11,14 +11,17 @@ import { project } from 'app/model';
 
         return (
             <Panel>
-                <Form>
-                    <Field label="width">
-                        <NumberInput value={project.scene.width} onChange={(val) => { project.scene.width = val; }} />
-                    </Field>
-                    <Field label="height">
-                        <NumberInput value={project.scene.height} onChange={(val) => { project.scene.height = val; }} />
-                    </Field>
-                </Form>
+                <Title>Scene properties</Title>
+                <Alt>
+                    <Form>
+                        <Field label="width">
+                            <NumberInput value={project.scene.width} onChange={(val) => { project.scene.width = val; }} />
+                        </Field>
+                        <Field label="height">
+                            <NumberInput value={project.scene.height} onChange={(val) => { project.scene.height = val; }} />
+                        </Field>
+                    </Form>
+                </Alt>
             </Panel>
         );
 
