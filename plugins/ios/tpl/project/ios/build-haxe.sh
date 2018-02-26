9d79bcf447d77625857dc7d747b52fe3b79df5e2
jeremyfa <contact@jeremyfa.com>
2018-02-26 01:24:47 +0100

    [ios] Auto-update project setup when running from Xcode

--- plugins/ios/tpl/project/ios/build-haxe.sh
+++ plugins/ios/tpl/project/ios/build-haxe.sh
@@ -16,7 +16,7 @@ if [ -d project/assets/assets ]; then rm -Rf project/assets/assets; fi
 if [ -d ../../out/luxe/ios/ios.project/project/assets/assets ]; then rm -Rf ../../out/luxe/ios/ios.project/project/assets/assets; fi
 
 # Build
-ceramic luxe build ios "$CERAMIC_DEBUG_FLAG" --cwd ../.. --archs "${ARCHS// /,}" --no-colors
+ceramic luxe build ios "$CERAMIC_DEBUG_FLAG" --cwd ../.. --archs "${ARCHS// /,}" --no-colors --setup
 
 # Copy new derived assets to Xcode project resources
 cp -r ../../out/luxe/ios/ios.project/project/assets/assets project/assets/
