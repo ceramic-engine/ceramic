98c930a4ab0a791c51c020df8b6b3007ac026a0a
jeremyfa <contact@jeremyfa.com>
2018-03-13 16:45:56 +0100

    [runtime] Add some more useful Array static extensions

--- runtime/src/ceramic/Extensions.hx
+++ runtime/src/ceramic/Extensions.hx
@@ -27,6 +27,70 @@ class Extensions {
 #end
     } //unsafeSet
 
+    /** Return a random element contained in the given array */
+    inline public static function randomElement<T>(array:Array<T>):T {
+
+        return array[Math.floor(Math.random() * 0.99999 * array.length)];
+
+    } //randomElement
+
+    /** Return a random element contained in the given array that is not equal to the `except` arg.
+        @param array  The array in which we extract the element from
+        @param except The element we don't want
+        @param unsafe If set to `true`, will prevent allocating a new array (and may be faster) but will loop forever if there is no element except the one we don't want
+        @return The random element or `null` if nothing was found */
+    public static function randomElementExcept<T>(array:Array<T>, except:T, unsafe:Bool = false):T {
+
+        if (unsafe) {
+            // Unsafe
+            var ret = null;
+
+            do {
+                ret = randomElement(array);
+            } while (ret == except);
+
+            return ret;
+        }
+        else {
+            // Safe
+
+            // Work on a copy
+            var array = array.copy();
+
+            // Shuffle array
+            shuffle(array);
+
+            // Get first item different than `except`
+            for (item in array) {
+                if (item != except) return item;
+            }
+        }
+
+        return null;
+
+    } //randomElement
+
+    /** Shuffle an Array. This operation affects the array in place.
+        The shuffle algorithm used is a variation of the [Fisher Yates Shuffle](http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) */
+	public static function shuffle<T>(arr:Array<T>):Void
+	{
+        inline function int(from:Int, to:Int):Int
+        {
+            return from + Math.floor(((to - from + 1) * Math.random()));
+        }
+
+		if (arr != null) {
+			for (i in 0...arr.length) {
+				var j = int(0, arr.length - 1);
+				var a = arr[i];
+				var b = arr[j];
+				arr[i] = b;
+				arr[j] = a;
+			}
+		}
+
+	} //shuffle
+
 /// Generic extensions
 
     @:noCompletion
