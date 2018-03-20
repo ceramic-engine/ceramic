c89fea5cf4632811de49ff4aa0e31791f93f6d3d
jeremyfa <contact@jeremyfa.com>
2018-03-20 02:04:28 +0100

    [runtime] Add Utils.uniqueId()

--- runtime/src/ceramic/Utils.hx
+++ runtime/src/ceramic/Utils.hx
@@ -26,4 +26,34 @@ class Utils {
 
 	} //getRtti
 
+    inline public static function uniqueId():String {
+
+        return base62Id() + base62Id() + base62Id() + base62Id() + base62Id() + base62Id() + base62Id() + base62Id();
+
+    } //uniqueId
+
+    inline public static function base62Id(?val:Null<Int>):String {
+
+        // http://www.anotherchris.net/csharp/friendly-unique-id-generation-part-2/#base62
+        // Haxe snippet from Luxe
+
+        if (val == null) {
+            val = Std.random(0x7fffffff);
+        }
+
+        function toChar(value:Int):String {
+            if (value > 9) {
+                var ascii = (65 + (value - 10));
+                if (ascii > 90) { ascii += 6; }
+                return String.fromCharCode(ascii);
+            } else return Std.string(value).charAt(0);
+        } //toChar
+
+        var r = Std.int(val % 62);
+        var q = Std.int(val / 62);
+        if (q > 0) return base62Id(q) + toChar(r);
+        else return Std.string(toChar(r));
+
+    } //base62Id
+
 } //Utils
