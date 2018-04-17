dbb11f0dee3614d90bd0b26e233b71f725b7b469
jeremyfa <contact@jeremyfa.com>
2018-04-17 18:57:39 +0200

    [runtime] Add Autorun.unobserve() and Autorun.reobserve(), which basically does unobserved(cb), but inlined.

--- runtime/src/ceramic/Autorun.hx
+++ runtime/src/ceramic/Autorun.hx
@@ -6,6 +6,8 @@ class Autorun extends Entity {
 
 /// Current autorun
 
+    static var prevCurrent:Array<Autorun> = [];
+
     public static var current:Autorun = null;
 
 /// Events
@@ -63,18 +65,34 @@ class Autorun extends Entity {
 
 /// Static helpers
 
+    /** Ensures current `autorun` won't be affected by the code after this call.
+        `reobserve()` should be called to restore previous state. */
+    inline public static function unobserve():Void {
+
+        // Set current autorun to null
+        prevCurrent.push(current);
+        current = null;
+
+    } //unobserve
+
+    /** Resume observing values and resume affecting current `autorun` scope.
+        This should be called after an `unobserve()` call. */
+    inline public static function reobserve():Void {
+
+        Assert.assert(prevCurrent.length > 0, 'Cannot call reobserve() without calling observe() before.');
+
+        // Restore previous current autorun
+        current = prevCurrent.pop();
+
+    } //reobserve
+
     /** Executes the given function synchronously and ensures the
         current `autorun` scope won't be affected */
     public static function unobserved(func:Void->Void):Void {
 
-        // Set current autorun to null
-        var prevCurrent = current;
-        current = null;
-
+        unobserve();
         func();
-
-        // Restore previous current autorun
-        current = prevCurrent;
+        reobserve();
 
     } //unobserved
 
