6789d14773d1d3f839586dc9f824adf71bb43e2e
jeremyfa <contact@jeremyfa.com>
2018-10-14 18:06:23 +0200

    [runtime] Add ValueEntry to create CollectionEntry object that can hold any typed value

---
+++ runtime/src/ceramic/ValueEntry.hx
@@ -0,0 +1,16 @@
+package ceramic;
+
+/** A collection entry that can hold any value */
+class ValueEntry<T> extends CollectionEntry {
+
+    public var value:T;
+
+    public function new(value:T, ?id:String, ?name:String) {
+
+        super(id, name);
+
+        this.value = value;
+
+    } //new
+
+} //ValueEntry
