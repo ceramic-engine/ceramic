5d946bacf1f5da0aa7a96c52b96c07e33af4b2f9
jeremyfa <contact@jeremyfa.com>
2019-02-05 11:46:48 +0100

    [tools] Update hxcpp, patch its ios toolchain at install, after all

--- tools/post-install.js
+++ tools/post-install.js
@@ -17,13 +17,21 @@ function postInstall() {
 
     // Install dependencies
     spawnSync(haxelib, ['install', 'hxnodejs', '4.0.9', '--always'], { stdio: "inherit", cwd: __dirname });
-    spawnSync(haxelib, ['install', 'hxcpp', '4.0.4', '--always'], { stdio: "inherit", cwd: __dirname });
+    spawnSync(haxelib, ['install', 'hxcpp', '4.0.8', '--always'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['install', 'bind', '0.4.1', '--always'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['install', 'unifill', '0.4.1', '--always'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['dev', 'spine-hx', '../git/spine-hx'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['dev', 'polyline', '../git/polyline'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['dev', 'actuate', '../git/actuate'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['install', 'build.hxml', '--always'], { stdio: "inherit", cwd: __dirname });
+
+    // Patch hxcpp toolchain on iOS
+    // See: https://github.com/HaxeFoundation/hxcpp/issues/764
+    var hxcppPath = (''+spawnSync(haxelib, ['path', 'hxcpp']).stdout).split("\n")[0].trim();
+    var iphoneToolchainPath = path.join(hxcppPath, 'toolchain/iphoneos-toolchain.xml');
+    var iphoneToolchain = '' + fs.readFileSync(iphoneToolchainPath);
+    iphoneToolchain = iphoneToolchain.split('<flag value="-O2" unless="debug"/>').join('<flag value="-O1" unless="debug"/>');
+    fs.writeFileSync(iphoneToolchainPath, iphoneToolchain);
     
     // Build tools
     spawnSync(haxe, ['build.hxml'], { stdio: "inherit", cwd: __dirname });
