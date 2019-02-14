b993a7a225cda24c7701d8d6b2f4dd320aa19626
Jeremy Faivre <contact@jeremyfa.com>
2019-02-14 11:02:25 +0100

    [runtime] Add BezierEasing utility

---
+++ runtime/src/ceramic/BezierEasing.hx
@@ -0,0 +1,137 @@
+package ceramic;
+
+import ceramic.Assert.*;
+
+/** Bezier curve easing, ported from https://github.com/gre/bezier-easing */
+class BezierEasing {
+
+    static var SPLINE_TABLE_SIZE = 11;
+    static var SAMPLE_STEP_SIZE = 1.0 / (SPLINE_TABLE_SIZE - 1.0);
+    static var NEWTON_ITERATIONS = 4;
+    static var NEWTON_MIN_SLOPE = 0.001;
+    static var SUBDIVISION_PRECISION = 0.0000001;
+    static var SUBDIVISION_MAX_ITERATIONS = 10;
+
+    var linearEasing = false;
+
+    var sampleValues:Array<Float>;
+
+    var mX1:Float;
+    
+    var mY1:Float;
+
+    var mX2:Float;
+
+    var mY2:Float;
+
+    public function new(x1:Float, y1:Float, x2:Float, y2:Float) {
+
+        this.mX1 = x1;
+        this.mY1 = y1;
+        this.mX2 = x2;
+        this.mY2 = y2;
+
+        assert((0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1), 'bezier x values must be in [0, 1] range');
+
+        if (mX1 == mY1 && mX2 == mY2) {
+            linearEasing = true;
+        }
+        else {
+            // Precompute samples table
+            sampleValues = [];
+            for (i in 0...SPLINE_TABLE_SIZE) {
+                sampleValues[i] = calcBezier(i * SAMPLE_STEP_SIZE, mX1, mX2);
+            }
+        }
+
+    } //new
+
+    public function ease(x:Float):Float {
+
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        return calcBezier(getTForX(x), mY1, mY2);
+
+    } //ease
+
+    inline function getTForX(aX:Float):Float {
+
+        var intervalStart = 0.0;
+        var currentSample = 1;
+        var lastSample = SPLINE_TABLE_SIZE - 1;
+
+        while (currentSample != lastSample && sampleValues[currentSample] <= aX) {
+            intervalStart += SAMPLE_STEP_SIZE;
+            currentSample++;
+        }
+        currentSample--;
+
+        // Interpolate to provide an initial guess for t
+        var dist = (aX - sampleValues[currentSample]) / (sampleValues[currentSample + 1] - sampleValues[currentSample]);
+        var guessForT:Float = intervalStart + dist * SAMPLE_STEP_SIZE;
+
+        var initialSlope = getSlope(guessForT, mX1, mX2); 
+        if (initialSlope >= NEWTON_MIN_SLOPE) {
+            return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
+        } else if (initialSlope == 0.0) {
+            return guessForT;
+        } else {
+            return binarySubdivide(aX, intervalStart, intervalStart + SAMPLE_STEP_SIZE, mX1, mX2);
+        }
+
+    } //aX
+
+    /** Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2 */
+    inline function calcBezier(aT:Float, aA1:Float, aA2:Float) {
+
+        return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+
+    } //calcBezier
+
+    /** Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2 **/
+    inline function getSlope(aT:Float, aA1:Float, aA2:Float) {
+        
+        return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+        
+    } //getSlope
+
+    inline function binarySubdivide(aX:Float, aA:Float, aB:Float, mX1:Float, mX2:Float) {
+
+        var currentX:Float;
+        var currentT:Float;
+        var i = 0;
+
+        do {
+            currentT = aA + (aB - aA) / 2.0;
+            currentX = calcBezier(currentT, mX1, mX2) - aX;
+            if (currentX > 0.0) {
+                aB = currentT;
+            } else {
+                aA = currentT;
+            }
+        } while (Math.abs(currentX) > SUBDIVISION_PRECISION && ++i < SUBDIVISION_MAX_ITERATIONS);
+
+        return currentT;
+
+    } //binarySubdivide
+
+    function newtonRaphsonIterate(aX:Float, aGuessT:Float, mX1:Float, mX2:Float) {
+
+        for (i in 0...NEWTON_ITERATIONS) {
+            var currentSlope = getSlope(aGuessT, mX1, mX2);
+            if (currentSlope == 0.0) {
+                return aGuessT;
+            }
+            var currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+            aGuessT -= currentX / currentSlope;
+        }
+
+        return aGuessT;
+
+    } //newtonRaphsonIterate
+
+    inline function A(aA1:Float, aA2:Float) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
+    inline function B(aA1:Float, aA2:Float) { return 3.0 * aA2 - 6.0 * aA1; }
+    inline function C(aA1:Float)            { return 3.0 * aA1; }
+
+} //BezierEasing
