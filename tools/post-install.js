3d6253f5986479977d705afa8978db60f6a2da6d
Jeremy Faivre <contact@jeremyfa.com>
2019-02-15 14:42:04 +0100

    Add differ dependency (for future polygon hit detection)

--- tools/post-install.js
+++ tools/post-install.js
@@ -23,6 +23,7 @@ function postInstall() {
     spawnSync(haxelib, ['dev', 'spine-hx', '../git/spine-hx'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['dev', 'polyline', '../git/polyline'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['dev', 'actuate', '../git/actuate'], { stdio: "inherit", cwd: __dirname });
+    spawnSync(haxelib, ['dev', 'differ', '../git/differ'], { stdio: "inherit", cwd: __dirname });
     spawnSync(haxelib, ['install', 'build.hxml', '--always'], { stdio: "inherit", cwd: __dirname });
 
     // Patch hxcpp toolchain on iOS
