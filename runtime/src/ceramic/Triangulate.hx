24e44e2f5746ef1059ff02c04c1925a70dae9cb4
Jeremy Faivre <contact@jeremyfa.com>
2019-04-19 17:41:10 +0200

    [runtime] Add utiltiy to triangulate indices from a vertex list
    Useful to draw mesh-based shapes or to create complex hitboxes (once we got a pointInTriangle() test)

---
+++ runtime/src/ceramic/Triangulate.hx
@@ -0,0 +1,25 @@
+package ceramic;
+
+import earcut.Earcut;
+
+/** An utility to triangulate indices from a set of vertices */
+class Triangulate {
+
+    /** Triangulate the given vertices and fills the indices array accordingly */
+    public static function triangulate(vertices:Array<Float>, indices:Array<Int>, ?holes:Array<Int>):Void {
+
+        // Empty indices data
+        if (indices.length > 0) {
+            #if cpp
+            untyped indices.__SetSize(0);
+            #else
+            indices.splice(0, indices.length);
+            #end
+        }
+
+        // Perform triangulation
+        Earcut.earcut(vertices, holes, 2, indices);
+
+    } //triangulate
+
+} //Triangulate
