e282d527132e28f952f2ea2f40ccb97d95311006
Jeremy Faivre <contact@jeremyfa.com>
2019-04-19 18:21:44 +0200

    [runtime] Making it public might be handy 😬

--- runtime/src/ceramic/GeometryUtils.hx
+++ runtime/src/ceramic/GeometryUtils.hx
@@ -4,7 +4,7 @@ package ceramic;
 class GeometryUtils {
 
     /** Returns `true` if the point `(x,y)` is inside the given (a,b,c) triangle */
-    static inline function pointInTriangle(x:Float, y:Float, ax:Float, ay:Float, bx:Float, by:Float, cx:Float, cy:Float):Bool {
+    public static inline function pointInTriangle(x:Float, y:Float, ax:Float, ay:Float, bx:Float, by:Float, cx:Float, cy:Float):Bool {
         return (cx - x) * (ay - y) - (ax - x) * (cy - y) >= 0 &&
                (ax - x) * (by - y) - (bx - x) * (ay - y) >= 0 &&
                (bx - x) * (cy - y) - (cx - x) * (by - y) >= 0;
