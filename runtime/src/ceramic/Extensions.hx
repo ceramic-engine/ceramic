e7718ffbefbd2f64ceb712f3d723994c22cab6c5
Jeremy Faivre <contact@jeremyfa.com>
2019-05-09 17:40:51 +0200

    [runtime] Actually, just use setProperty() instead of some fragile target-specific checks. That works.

--- runtime/src/ceramic/Extensions.hx
+++ runtime/src/ceramic/Extensions.hx
@@ -132,29 +132,14 @@ class Extensions<T> {
     @:noCompletion
     inline public static function setProperty<T>(instance:T, field:String, value:Dynamic):Void {
 
-        // @see https://github.com/openfl/actuate/blob/4547a5a6d2e95dbb3f6b8eacb719532b4c1650d2/motion/actuators/SimpleActuator.hx#L327-L345
-		if (Reflect.hasField(instance, field) #if flash && !untyped (instance).hasOwnProperty("set_" + field) #elseif js && !(untyped (instance).__properties__ && untyped (instance).__properties__["set_" + field]) #end) {
-			#if flash
-			untyped target[field] = value;
-			#else
-			Reflect.setField(instance, field, value);
-			#end
-		}
-        else {
-			Reflect.setProperty(instance, field, value);
-		}
+        Reflect.setProperty(instance, field, value);
 
     } //setProperty
 
     @:noCompletion
     inline public static function getProperty<T>(instance:T, field:String):Dynamic {
 
-        if (#if flash untyped (instance).hasOwnProperty ("get_" + field) #elseif js untyped (instance).__properties__ && untyped (instance).__properties__["get_" + field] #else false #end) {
-            return Reflect.getProperty(instance, field);
-        }
-        else {
-            return Reflect.field(instance, field);
-        }
+        return Reflect.getProperty(instance, field);
 
     } //getProperty
 
