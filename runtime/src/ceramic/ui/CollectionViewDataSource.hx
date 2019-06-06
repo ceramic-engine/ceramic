badf68b36d11893c248e69a249f5bc63704d720c
Jeremy Faivre <contact@jeremyfa.com>
2019-06-06 16:23:17 +0200

    [runtime] Add collectionView instance as first argument in CollectionViewDataSource

--- runtime/src/ceramic/ui/CollectionViewDataSource.hx
+++ runtime/src/ceramic/ui/CollectionViewDataSource.hx
@@ -3,18 +3,18 @@ package ceramic.ui;
 interface CollectionViewDataSource {
 
     /** Get the number of elements. */
-    function collectionViewSize():Int;
+    function collectionViewSize(collectionView:CollectionViewcollectionView:CollectionView):Int;
 
     /** Get the item frame at the requested index. */
-    function collectionViewItemFrameAtIndex(itemIndex:Int, frame:CollectionViewItemFrame):Void;
+    function collectionViewItemFrameAtIndex(collectionView:CollectionView, itemIndex:Int, frame:CollectionViewItemFrame):Void;
 
     /** Called when a view is not used anymore at the given index. Lets the dataSource
         do some cleanup if needed, before this view gets reused (if it can).
         Returns `true` if the view can be reused at another index of `false` otherwise. */
-    function collectionViewReleaseItemAtIndex(itemIndex:Int, view:View):Bool;
+    function collectionViewReleaseItemAtIndex(collectionView:CollectionView, itemIndex:Int, view:View):Bool;
 
     /** Get a view at the given index. If `reusableView` is provided,
         it can be recycled as the new item to avoid creating new instances. */
-    function collectionViewItemAtIndex(itemIndex:Int, reusableView:View):View;
+    function collectionViewItemAtIndex(collectionView:CollectionView, itemIndex:Int, reusableView:View):View;
 
 } //CollectionViewDataSource
