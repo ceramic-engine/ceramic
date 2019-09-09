2616ec7fdd432206631f51c509e9fe32d6933224
Jeremy Faivre <contact@jeremyfa.com>
2019-09-10 00:42:44 +0200

    [luxe] Fix compilation failing web debug is true

--- plugins/luxe/runtime/src/backend/TextureId.hx
+++ plugins/luxe/runtime/src/backend/TextureId.hx
@@ -4,6 +4,6 @@ import phoenix.Texture;
 
 abstract TextureId(phoenix.TextureID) from phoenix.TextureID to phoenix.TextureID {
 
-    inline public static var DEFAULT:TextureId = (#if snow_web null #else 0 #end : phoenix.TextureID);
+    #if !debug inline #end public static var DEFAULT:TextureId = (#if snow_web null #else 0 #end : phoenix.TextureID);
 
 } //TextureId
