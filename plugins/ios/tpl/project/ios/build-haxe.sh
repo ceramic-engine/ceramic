2d738477ed57bee2547072262267101cc0e29d2c
Jeremy Faivre <contact@jeremyfa.com>
2019-09-12 19:01:50 +0200

    [ios] Update project template to match recent changes

--- plugins/ios/tpl/project/ios/build-haxe.sh
+++ plugins/ios/tpl/project/ios/build-haxe.sh
@@ -12,12 +12,5 @@ fi
 # Generate ceramic app assets
 ceramic luxe assets ios --no-colors --cwd ../..
 
-# Remove derived assets from flow
-if [ -d project/assets/assets ]; then rm -Rf project/assets/assets; fi
-if [ -d ../../out/luxe/ios/ios.project/project/assets/assets ]; then rm -Rf ../../out/luxe/ios/ios.project/project/assets/assets; fi
-
 # Build
 ceramic luxe build ios "$CERAMIC_DEBUG_FLAG" --cwd ../.. --archs "${ARCHS// /,}" --no-colors --setup
-
-# Copy new derived assets to Xcode project resources
-cp -r ../../out/luxe/ios/ios.project/project/assets/assets project/assets/
