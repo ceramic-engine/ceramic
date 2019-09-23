a6d53aba3723a62b873bd0d6a09a7c821028b225
Jeremy Faivre <contact@jeremyfa.com>
2019-09-23 12:22:14 +0200

    [tools] Ensure tools point to correct haxe binaries on windows

--- tools/haxelib.cmd
+++ tools/haxelib.cmd
@@ -1,4 +1,6 @@
 
 @echo off
 SET PATH=%~dp0;%PATH%
-%~dp0/node_modules/.bin/node %~dp0/node_modules/haxe/bin/haxelib %*
+SET PATH=%~dp0/../git/haxe-binary/windows/neko;%PATH%
+SET PATH=%~dp0/../git/haxe-binary/windows/haxe;%PATH%
+%~dp0/../git/haxe-binary/windows/haxe/haxelib %*
--- tools/neko.cmd
+++ tools/neko.cmd
@@ -1,3 +1,6 @@
 
 @echo off
-%~dp0/node_modules/.bin/node %~dp0/node_modules/haxe/bin/neko %*
+SET PATH=%~dp0;%PATH%
+SET PATH=%~dp0/../git/haxe-binary/windows/neko;%PATH%
+SET PATH=%~dp0/../git/haxe-binary/windows/haxe;%PATH%
+%~dp0/../git/haxe-binary/windows/neko/neko %*
