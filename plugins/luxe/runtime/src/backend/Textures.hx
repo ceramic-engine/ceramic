6f6f753b985ee83308c2d88e8aaaa1e8c602cda6
Jeremy Faivre <contact@jeremyfa.com>
2019-10-11 19:17:07 +0200

    [backend] Just adding a useful log

--- plugins/luxe/runtime/src/backend/Textures.hx
+++ plugins/luxe/runtime/src/backend/Textures.hx
@@ -261,6 +261,9 @@ class Textures implements spec.Textures {
             var maxUnits:Array<Int> = [0];
             GL.glGetIntegerv(GL.GL_MAX_TEXTURE_IMAGE_UNITS, maxUnits);
             _maxTexturesByBatch = Std.int(Math.min(32, maxUnits[0]));
+
+            trace('computed max textures by batch: ' + _maxTexturesByBatch);
+
             #else
             _maxTexturesByBatch = Std.int(Math.min(32, GL.getParameter(GL.MAX_TEXTURE_IMAGE_UNITS)));
             #end
