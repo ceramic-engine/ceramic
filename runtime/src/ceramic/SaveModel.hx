5e23061a16cf735329bb3c5a25879fc8a3e72f64
Jeremy Faivre <contact@jeremyfa.com>
2019-11-16 16:47:16 +0100

    [runtime] Improve SaveModel so that it automatically creates backups that it can fall back on in case of data corruption

--- runtime/src/ceramic/SaveModel.hx
+++ runtime/src/ceramic/SaveModel.hx
@@ -5,9 +5,15 @@ import ceramic.Assert.*;
 
 class SaveModel {
 
-    inline static var NUM_BACKUPS = 5;
+    static var BACKUP_NUM_STEPS:Int = 20000;
 
-    static var saveStepByKey:Map<String,Int> = new Map();
+    static var NUM_BACKUPS:Int = 4;
+
+    static var BACKUP_STEPS:Array<Int> = null;
+
+    static var backupStepByKey:Map<String,Int> = null;
+
+    static var busyKeys:Array<String> = [];
 
     static var backgroundQueue:BackgroundQueue = null;
 
@@ -25,19 +31,43 @@ class SaveModel {
 
     } //modelClass
 
+    /** Load data from the given key. */
     public static function loadFromKey(model:Model, key:String):Bool {
 
-        var id = Std.parseInt(app.backend.io.readString('save_id_1_' + key));
-        if (id != 1 && id != 2) {
-            id = Std.parseInt(app.backend.io.readString('save_id_2_' + key));
+        if (busyKeys.indexOf(key) != -1) {
+            throw 'Cannot load data from key $key because some work is being done on it';
         }
 
+        initBackupLogicIfNeeded(key);
+
+        var rawId = app.backend.io.readString('save_id_1_' + key);
+        var id = rawId != null ? Std.parseInt(rawId) : -1;
         if (id != 1 && id != 2) {
-            warning('Failed to load save from key: $key (no existing save)');
-            return false;
+            rawId = app.backend.io.readString('save_id_2_' + key);
+            id = rawId != null ? Std.parseInt(rawId) : -1;
         }
 
-        var data = app.backend.io.readString('save_data_' + id + '_' + key);
+        var data:String = null;
+
+        if (id != 1 && id != 2) {
+            warning('Failed to load save from key: $key (no existing save?)');
+        }
+        else {
+            data = app.backend.io.readString('save_data_' + id + '_' + key);
+            if (data == null) {
+                warning('Failed to load save from key: $key/$id (corrupted save, try backups?)');
+            }
+        }
+
+        if (data == null) {
+            data = fetchMostRecentBackup(key);
+            if (data == null) {
+                warning('No backup available for key $key, that is probably a new save slot.');
+            }
+            else {
+                success('Recovered from backup!');
+            }
+        }
 
         return loadFromData(model, data);
 
@@ -96,6 +126,9 @@ class SaveModel {
         // Init background queue if needed
         if (backgroundQueue == null) backgroundQueue = new BackgroundQueue();
 
+        // Init backup logic if needed
+        initBackupLogicIfNeeded(key);
+
         if (model.serializer != null) {
             model.serializer.destroy();
             model.serializer = null;
@@ -113,12 +146,18 @@ class SaveModel {
         // Start listening for changes to save them
         serializer.onChangeset(model, function(changeset) {
 
+            // Mark this key as busy
+            busyKeys.push(key);
+
             if (changeset.append) {
+
+                // Append
+                //
                 #if ceramic_debug_save
                 trace('Save $key (append ${changeset.data.length})');//: ' + changeset.data);
                 #end
 
-                (function(data:String) {
+                (function(data:String, key:String) {
                     backgroundQueue.schedule(function() {
 
                         // We use and update multiple files to ensure that, in case of crash or any other issue
@@ -139,15 +178,32 @@ class SaveModel {
                         // Mark this second file as the valid one on second id key
                         app.backend.io.saveString(saveIdKey2, '2');
 
+                        Runner.runInMain(function() {
+                            // Pop busy key
+                            var busyIndex = busyKeys.indexOf(key);
+                            if (busyIndex != -1) {
+                                busyKeys.splice(busyIndex, 1);
+                            }
+                            else {
+                                error('Failed to remove busy key: $key (none in list)');
+                            }
+                        });
+
                     });
-                })(changeset.data);
+                })(changeset.data, key);
 
             } else {
+
+                // Compact
+                //
                 #if ceramic_debug_save
                 trace('Save $key (full ${changeset.data.length})');//: ' + changeset.data);
                 #end
+
+                var backupStep = backupStepByKey.get(key);
+                var backupId = BACKUP_STEPS[backupStep];
                 
-                (function(data:String) {
+                (function(data:String, key:String, backupStep:Int, backupId:Int) {
                     backgroundQueue.schedule(function() {
 
                         // We use and update multiple files to ensure that, in case of crash or any other issue
@@ -168,9 +224,35 @@ class SaveModel {
                         // Mark this second file as the valid one on second id key
                         app.backend.io.saveString(saveIdKey2, '2');
 
+                        // Save a backup on compact
+                        // That file will be used at load if we fail to load the regular one
+                        app.backend.io.saveString('backup_data_' + backupId + '_' + key, Math.round(Date.now().getTime()) + ':' + data.length + ':' + data);
+
+                        // Increment backup step
+                        backupStep = (backupStep + 1) % BACKUP_NUM_STEPS;
+
+                        // Update backup step on disk
+                        app.backend.io.saveString('backup_step_1_' + key, '' + backupStep);
+                        app.backend.io.saveString('backup_step_2_' + key, '' + backupStep);
+
+                        Runner.runInMain(function() {
+
+                            // Update backup step in map
+                            backupStepByKey.set(key, backupStep);
+
+                            // Pop busy key
+                            var busyIndex = busyKeys.indexOf(key);
+                            if (busyIndex != -1) {
+                                busyKeys.splice(busyIndex, 1);
+                            }
+                            else {
+                                error('Failed to remove busy key: $key (none in list)');
+                            }
+                        });
+                        
 
                     });
-                })(changeset.data);
+                })(changeset.data, key, backupStep, backupId);
             }
 
         });
@@ -245,5 +327,86 @@ class SaveModel {
         };
 
     } //decodeData
+
+    static function initBackupLogicIfNeeded(key:String):Void {
+
+        if (backupStepByKey == null) {
+            backupStepByKey = new Map();
+
+            BACKUP_STEPS = Utils.uniformFrequencyList(
+                [1, 2, 3, 4],
+                [
+                    0.5 - 20.0 / BACKUP_NUM_STEPS - 2.0 / BACKUP_NUM_STEPS,
+                    0.5 - 20.0 / BACKUP_NUM_STEPS - 2.0 / BACKUP_NUM_STEPS,
+                    20.0 / BACKUP_NUM_STEPS,
+                    2.0 / BACKUP_NUM_STEPS
+                ],
+                BACKUP_NUM_STEPS
+            );
+        }
+        
+        if (!backupStepByKey.exists(key)) {
+
+            var rawStep = app.backend.io.readString('backup_step_1_' + key);
+            var step = rawStep != null ? Std.parseInt(rawStep) : -1;
+            if (step == null || Math.isNaN(step) || step < 0 || step >= BACKUP_NUM_STEPS) {
+                rawStep = app.backend.io.readString('backup_step_2_' + key);
+                step = rawStep != null ? Std.parseInt(rawStep) : -1;
+            }
+
+            if (step == null || Math.isNaN(step) || step < 0 || step >= BACKUP_NUM_STEPS) {
+                warning('No backup step saved, start with zero');
+                step = 0;
+            }
+
+            backupStepByKey.set(key, step);
+        }
+
+    } //initBackupLogicIfNeeded
+
+    static function fetchMostRecentBackup(key:String):String {
+
+        var backups:Array<String> = [];
+        var times:Array<Float> = []; 
+
+        for (backupId in 0...4) {
+            var backup = app.backend.io.readString('backup_data_' + backupId + '_' + key);
+
+            if (backup != null) {
+                // Extract time and data
+                var colonIndex = backup.indexOf(':');
+                if (colonIndex != -1) {
+                    var rawTime = backup.substring(0, colonIndex);
+                    var time:Null<Float> = Std.parseFloat(rawTime);
+                    if (time != null && !Math.isNaN(time) && time > 0) {
+                        backups.push(backup.substring(colonIndex + 1));
+                        times.push(time);
+                    }
+                }
+            } 
+        }
+
+        var bestTime:Float = -1;
+        var bestIndex:Int = -1;
+
+        // Find most rencent backup among every loaded backup
+        for (i in 0...times.length) {
+            var time = times[i];
+            if (time > bestTime) {
+                bestTime = time;
+                bestIndex = i;
+            }
+        }
+
+        if (bestIndex != -1) {
+            // Found one!
+            return backups[bestIndex];
+        }
+        else {
+            // No backup available
+            return null;
+        }
+
+    } //fetchMostRecentBackup
 
 } //SaveModel
