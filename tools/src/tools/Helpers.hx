5647b7c1555be21f1c54a6cd62ae637f39ad2026
Jeremy Faivre <contact@jeremyfa.com>
2020-01-21 13:11:03 +0100

    [tools] Update bind dependency to 0.4.4 (with a java parsing bug fix)

--- tools/src/tools/Helpers.hx
+++ tools/src/tools/Helpers.hx
@@ -336,7 +336,7 @@ class Helpers {
         if (!FileSystem.exists(haxelibRepoPath)) {
             FileSystem.createDirectory(haxelibRepoPath);
             haxelib(['install', 'hxcpp', '4.0.52', '--always'], {cwd: cwd});
-            haxelib(['install', 'bind', '0.4.3', '--always'], {cwd: cwd});
+            haxelib(['install', 'bind', '0.4.4', '--always'], {cwd: cwd});
             haxelib(['install', 'format', '3.4.2', '--always'], {cwd: cwd});
             haxelib(['install', 'unifill', '0.4.1', '--always'], {cwd: cwd});
             haxelib(['install', 'format', '3.4.2', '--always'], {cwd: cwd});
