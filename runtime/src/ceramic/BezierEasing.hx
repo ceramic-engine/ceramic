b9c472afc47698c311c3acd17765dbf737ec4082
Jeremy Faivre <contact@jeremyfa.com>
2020-01-24 23:28:16 +0100

    [runtime] BezierEasing improvements: accept quadratic params ; add cache size limit ; allow to reconfigure existing instances

--- runtime/src/ceramic/BezierEasing.hx
+++ runtime/src/ceramic/BezierEasing.hx
@@ -4,7 +4,8 @@ import ceramic.Assert.*;
 
 using ceramic.Extensions;
 
-/** Bezier curve easing, ported from https://github.com/gre/bezier-easing */
+/** Bezier curve easing, ported from https://github.com/gre/bezier-easing
+    then extended to work with both cubic and quadratic settings */
 class BezierEasing {
 
     static var SPLINE_TABLE_SIZE = 11;
@@ -13,10 +14,21 @@ class BezierEasing {
     static var NEWTON_MIN_SLOPE = 0.001;
     static var SUBDIVISION_PRECISION = 0.0000001;
     static var SUBDIVISION_MAX_ITERATIONS = 10;
+    static var TWO_THIRD = 2.0 / 3.0;
+
+    static var CACHE_SIZE:Int = 10000;
 
     var linearEasing = false;
 
     var sampleValues:Array<Float>;
+
+    var cached:Bool = false;
+
+    var quadratic:Bool = false;
+
+    var mQuadraticX1:Float;
+
+    var mQuadraticX2:Float;
 
     var mX1:Float;
     
@@ -26,12 +38,43 @@ class BezierEasing {
 
     var mY2:Float;
 
-    public function new(x1:Float, y1:Float, x2:Float, y2:Float) {
+    /** Create a new instance with the given arguments.
+        If only `x1` and `y1` are provided, the curve is treated as quadratic.
+        If all four values `x1`, `y1`, `x2`, `y2` are provided,
+        the curve is treated as cubic. */
+    public function new(x1:Float, y1:Float, ?x2:Float, ?y2:Float) {
 
-        this.mX1 = x1;
-        this.mY1 = y1;
-        this.mX2 = x2;
-        this.mY2 = y2;
+        inline configure(x1, y1, x2, y2);
+
+    } //new
+
+    /** Configure the instance with the given arguments.
+        If only `x1` and `y1` are provided, the curve is treated as quadratic.
+        If all four values `x1`, `y1`, `x2`, `y2` are provided,
+        the curve is treated as cubic. */
+    public function configure(x1:Float, y1:Float, ?x2:Float, ?y2:Float) {
+
+        // If this instance was part of the cache,
+        // it should be removed as its settings will change
+        if (cached)
+            removeFromCache(mX1, mY1, mX2, mY2);
+
+        if (x2 == null || y2 == null) {
+            this.quadratic = true;
+            this.mQuadraticX1 = x1;
+            this.mQuadraticX2 = x2;
+            this.mX1 = quadraticToCubicCP1(x1);
+            this.mY1 = quadraticToCubicCP1(y1);
+            this.mX2 = quadraticToCubicCP2(x1);
+            this.mY2 = quadraticToCubicCP2(y1);
+        }
+        else {
+            this.quadratic = false;
+            this.mX1 = x1;
+            this.mY1 = y1;
+            this.mX2 = x2;
+            this.mY2 = y2;
+        }
 
         assert((0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1), 'bezier x values must be in [0, 1] range');
 
@@ -40,13 +83,14 @@ class BezierEasing {
         }
         else {
             // Precompute samples table
-            sampleValues = [];
+            if (sampleValues == null)
+                sampleValues = [];
             for (i in 0...SPLINE_TABLE_SIZE) {
                 sampleValues[i] = calcBezier(i * SAMPLE_STEP_SIZE, mX1, mX2);
             }
         }
 
-    } //new
+    } //configure
 
     public function ease(x:Float):Float {
 
@@ -136,40 +180,91 @@ class BezierEasing {
     inline function B(aA1:Float, aA2:Float) { return 3.0 * aA2 - 6.0 * aA1; }
     inline function C(aA1:Float)            { return 3.0 * aA1; }
 
+    inline static function quadraticToCubicCP1(p:Float):Float {
+
+        return TWO_THIRD * p;
+        
+    } //quadraticToCubicCP1
+
+    inline static function quadraticToCubicCP2(p:Float):Float {
+
+        return 1.0 + TWO_THIRD * (p - 1.0);
+        
+    } //quadraticToCubicCP2
+
     /// Cache
 
     static var cachedInstances:IntMap<Array<BezierEasing>> = null;
 
+    static var numCachedInstances:Int = 0;
+
+    function removeFromCache(x1:Float, y1:Float, x2:Float, y2:Float):Void {
+
+        cached = false;
+
+        var key = cacheKey(x1, y1, x2, y2);
+        if (cachedInstances != null) {
+            var list = cachedInstances.getInline(key);
+            if (list != null) {
+                if (list.length == 1 && list.unsafeGet(0) == this) {
+                    cachedInstances.remove(key);
+                }
+                else {
+                    list.remove(this);
+                }
+            }
+        }
+
+    } //removeFromCache
+
+    inline static function cacheKey(x1:Float, y1:Float, x2:Float, y2:Float):Int {
+
+        var floatKey = x1 * 100 + y1 * 1000 + x2 * 10000 + y2 * 100000;
+        return Std.int(floatKey);
+
+    } //cacheKey
+
     public static function clearCache():Void {
 
         cachedInstances = null;
+        numCachedInstances = 0;
 
     } //clearCache
 
     /** Get or create a `BezierEasing` instance with the given parameters.
         Created instances are cached and reused. */
-    public static function get(x1:Float, y1:Float, x2:Float, y2:Float):BezierEasing {
+    public static function get(x1:Float, y1:Float, ?x2:Float, ?y2:Float):BezierEasing {
+
+        var quadratic = (x2 == null || y2 == null);
+
+        var _x1:Float = quadratic ? quadraticToCubicCP1(x1) : x1;
+        var _y1:Float = quadratic ? quadraticToCubicCP1(y1) : y1;
+        var _x2:Float = quadratic ? quadraticToCubicCP2(x1) : x2;
+        var _y2:Float = quadratic ? quadraticToCubicCP2(y1) : y2;
 
         var result:BezierEasing = null;
 
-        var floatKey = x1 * 100 + y1 * 1000 + x2 * 10000 + y2 * 100000;
-        var key = Std.int(floatKey);
+        var key = cacheKey(_x1, _y1, _x2, _y2);
         if (cachedInstances == null) {
             cachedInstances = new IntMap<Array<BezierEasing>>();
         }
         var list = cachedInstances.getInline(key);
         if (list == null) {
+            if (numCachedInstances >= CACHE_SIZE) {
+                clearCache();
+            }
             // No list matching key, create new list with new instance
-            result = new BezierEasing(x1, y1, x2, y2);
+            result = new BezierEasing(_x1, _y1, _x2, _y2);
             cachedInstances.set(key, [result]);
+            numCachedInstances++;
         }
         else {
             // Look for an existing instance, starting from the latest added
             var i = list.length - 1;
             while (i >= 0) {
                 var instance = list.unsafeGet(i);
-                if (instance.mX1 == x1 && instance.mY1 == y1
-                && instance.mX2 == x2 && instance.mY2 == y2) {
+                if (instance.mX1 == _x1 && instance.mY1 == _y1
+                && instance.mX2 == _x2 && instance.mY2 == _y2) {
                     // Found it! reuse instance
                     result = instance;
                     break;
@@ -179,6 +274,7 @@ class BezierEasing {
             if (result == null) {
                 // Nothing found, create a new instance
                 result = new BezierEasing(x1, y1, x2, y2);
+                result.cached = true;
                 list.push(result);
             }
         }
