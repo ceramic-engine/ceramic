a703b01b07e92b1af5647ea3e4300fbc47f8ad56
Jeremy Faivre <contact@jeremyfa.com>
2020-03-09 13:54:17 +0100

    [tracker] Fixes to make tracker + ceramic cope better with haxe code completion

--- tools/src/tools/Project.hx
+++ tools/src/tools/Project.hx
@@ -157,12 +157,18 @@ class Project {
         app.hxml += "\n" + "-D app_info=" + Json.stringify(Json.stringify(app));
         app.hxml += "\n" + "--macro ceramic.macros.MacroCache.init()";
 
+        app.hxml += '\n' + '-D tracker_ceramic';
         app.hxml += '\n' + '-D tracker_no_default_backend';
-        app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setEntity("ceramic.Entity")';
-        app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setComponent("ceramic.Component")';
-        app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setBackend("ceramic.TrackerBackend")';
-        app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setArrayPool("ceramic.ArrayPool")';
-        app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setReusableArray("ceramic.ReusableArray")';
+        app.hxml += '\n' + '-D tracker_custom_entity=ceramic.Entity';
+        app.hxml += '\n' + '-D tracker_custom_component=ceramic.Component';
+        app.hxml += '\n' + '-D tracker_custom_array_pool=ceramic.ArrayPool';
+        app.hxml += '\n' + '-D tracker_custom_backend=ceramic.TrackerBackend';
+        app.hxml += '\n' + '-D tracker_custom_reusable_array=ceramic.ReusableArray';
+        // app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setEntity("ceramic.Entity")';
+        // app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setComponent("ceramic.Component")';
+        // app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setBackend("ceramic.TrackerBackend")';
+        // app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setArrayPool("ceramic.ArrayPool")';
+        // app.hxml += '\n' + '--macro tracker.macros.TrackerMacro.setReusableArray("ceramic.ReusableArray")';
 
         if (context.defines.exists('android')) {
             app.hxml += "\n" + "-D HXCPP_ANDROID_PLATFORM=26 -D PLATFORM=android-26";
