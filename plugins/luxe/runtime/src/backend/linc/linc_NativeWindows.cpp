ef2f101d0119156a4900b1a7027d14454b09b30d
Jeremy Faivre <contact@jeremyfa.com>
2020-04-04 16:52:20 +0200

    [windows] Better handling of hidpi screens

--- plugins/luxe/runtime/src/backend/linc/linc_NativeWindows.cpp
+++ plugins/luxe/runtime/src/backend/linc/linc_NativeWindows.cpp
@@ -1,14 +1,27 @@
 #include <hxcpp.h>
 #include "linc_NativeWindows.h"
 
+#include <Windows.h>
+#include <ShellScalingAPI.h>
+#include <comdef.h>
+
 // Link with SetupAPI.Lib.
-#pragma comment (lib, "setupapi.lib")
+#pragma comment (lib, "SetupAPI.lib")
+
+// Link with Shcore.lib.
+#pragma comment (lib, "Shcore.lib")
 
 namespace backend {
 
     void NativeWindows_init() {
 
-        // Nothing to do
+        // Enable HiDPI screen
+        HRESULT hr = SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+        if (FAILED(hr))
+        {
+            _com_error err(hr);
+            fwprintf(stderr, L"SetProcessDpiAwareness: %s\n", err.ErrorMessage());
+        }
 
     }
 
