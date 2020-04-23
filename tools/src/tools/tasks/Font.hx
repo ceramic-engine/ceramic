230519eadb61173e77bfe726210a238f1f109ce3
Jeremy Faivre <contact@jeremyfa.com>
2020-04-23 14:25:02 +0200

    [tools] Handle exporting fonts with multiple pages as well

--- tools/src/tools/tasks/Font.hx
+++ tools/src/tools/tasks/Font.hx
@@ -33,8 +33,6 @@ class Font extends Task {
 
     override function run(cwd:String, args:Array<String>):Void {
 
-        // TODO fonts with multiple pages
-
         var fontPath = extractArgValue(args, 'font');
         var outputPath = extractArgValue(args, 'out');
         var charset = extractArgValue(args, 'charset');
@@ -99,86 +97,6 @@ class Font extends Task {
             '-p', '2',
             '--smart-size'
         ], { cwd: tmpDir });
-        
-        var svgPath = Path.join([tmpDir, rawName + '.svg']);
-        var pngPath = Path.join([tmpDir, rawName + '.png']);
-        var flatPngPath = Path.join([tmpDir, rawName + '-flat.png']);
-
-        if (!msdf) {
-            // Generate a regular png texture from svg if msdf is disabled
-            Sync.run(function(done) {
-    
-                // Get png dimensions
-                sharp(
-                    pngPath
-                )
-                .raw()
-                .toBuffer(function(err, data, info) {
-    
-                    if (err != null) throw err;
-    
-                    var width:Float = info.width;
-                    var height:Float = info.height;
-
-                    // Somehow we may need to slightly adjust crop position to make it right
-                    var offsetX = 2;
-                    var offsetY = 0;
-    
-                    // Save svg as png with same dimensions
-                    sharp(
-                        svgPath
-                    )
-                    .extract({
-                        left: offsetX, top: offsetY, width: width, height: height
-                    })
-                    .toFile(
-                        flatPngPath,
-                        function(err, info) {
-                            if (err != null) throw err;
-                            done();
-                        }
-                    );
-    
-                });
-    
-            });
-
-            Sync.run(function(done) {
-
-                // Make the png white
-                sharp(
-                    flatPngPath
-                )
-                .raw()
-                .toBuffer(function(err, data, info:Dynamic) {
-                    var pixels:Uint8Array = data;
-                    var len = pixels.length;
-                    var i = 0;
-                    while (i < len) {
-                        pixels[i + 0] = 255;
-                        pixels[i + 1] = 255;
-                        pixels[i + 2] = 255;
-                        i += 4;
-                    }
-                    sharp(pixels, {
-                        raw: {
-                            width: info.width,
-                            height: info.height,
-                            channels: info.channels
-                        }
-                    })
-                    .png()
-                    .toFile(flatPngPath, function(err, info) {
-                        if (err != null) throw err;
-                        done();
-                    });
-                });
-
-            });
-
-            FileSystem.deleteFile(pngPath);
-            FileSystem.rename(flatPngPath, pngPath);
-        }
 
         // Extract font data
         var jsonPath = Path.join([tmpDir, rawName + '.json']);
@@ -221,8 +139,11 @@ class Font extends Task {
             fnt += '\n';
         }
 
+        var pngFiles = [];
         var i = 0;
         for (page in (json.pages:Array<String>)) {
+
+            pngFiles.push(page);
 
             var chars:Array<Dynamic> = json.chars;
 
@@ -264,10 +185,99 @@ class Font extends Task {
 
         }
 
+        // Create regular textures from svg files, if msdf is disabled
+        if (!msdf) {
+            for (pngFile in pngFiles) {
+                
+                var pngRawName = Path.withoutExtension(pngFile);
+        
+                var svgPath = Path.join([tmpDir, pngRawName + '.svg']);
+                var pngPath = Path.join([tmpDir, pngRawName + '.png']);
+                var flatPngPath = Path.join([tmpDir, pngRawName + '-flat.png']);
+
+                // Generate a regular png texture from svg if msdf is disabled
+                Sync.run(function(done) {
+        
+                    // Get png dimensions
+                    sharp(
+                        pngPath
+                    )
+                    .raw()
+                    .toBuffer(function(err, data, info) {
+        
+                        if (err != null) throw err;
+        
+                        var width:Float = info.width;
+                        var height:Float = info.height;
+
+                        // Somehow we may need to slightly adjust crop position to make it right
+                        var offsetX = 2;
+                        var offsetY = 0;
+        
+                        // Save svg as png with same dimensions
+                        sharp(
+                            svgPath
+                        )
+                        .extract({
+                            left: offsetX, top: offsetY, width: width, height: height
+                        })
+                        .toFile(
+                            flatPngPath,
+                            function(err, info) {
+                                if (err != null) throw err;
+                                done();
+                            }
+                        );
+        
+                    });
+        
+                });
+
+                Sync.run(function(done) {
+
+                    // Make the png white
+                    sharp(
+                        flatPngPath
+                    )
+                    .raw()
+                    .toBuffer(function(err, data, info:Dynamic) {
+                        var pixels:Uint8Array = data;
+                        var len = pixels.length;
+                        var i = 0;
+                        while (i < len) {
+                            pixels[i + 0] = 255;
+                            pixels[i + 1] = 255;
+                            pixels[i + 2] = 255;
+                            i += 4;
+                        }
+                        sharp(pixels, {
+                            raw: {
+                                width: info.width,
+                                height: info.height,
+                                channels: info.channels
+                            }
+                        })
+                        .png()
+                        .toFile(flatPngPath, function(err, info) {
+                            if (err != null) throw err;
+                            done();
+                        });
+                    });
+
+                });
+
+                FileSystem.deleteFile(pngPath);
+                FileSystem.rename(flatPngPath, pngPath);
+            }
+        }
+
         // Final export
         var fntPath = Path.join([outputPath, rawName + '.fnt']);
         File.saveContent(fntPath, fnt);
-        File.copy(pngPath, Path.join([outputPath, Path.withoutDirectory(pngPath)]));
+        for (pngFile in pngFiles) {
+            var pngPath = Path.join([tmpDir, pngFile]);
+            File.copy(pngPath, Path.join([outputPath, Path.withoutDirectory(pngPath)]));
+        }
 
         // Remove temporary files
         Files.deleteRecursive(tmpDir);
