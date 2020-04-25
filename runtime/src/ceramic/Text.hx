5c816ddc381a21aa2ddf7aaf39e3423325a103de
Jeremy Faivre <contact@jeremyfa.com>
2020-04-26 00:04:53 +0200

    [editor] Some more sliders in editor

--- runtime/src/ceramic/Text.hx
+++ runtime/src/ceramic/Text.hx
@@ -53,7 +53,7 @@ class Text extends Visual {
         return content;
     }
 
-    @editable
+    @editable({ slider: [6, 600] })
     public var pointSize(default, set):Float = 20;
     function set_pointSize(pointSize:Float):Float {
         if (this.pointSize == pointSize) return pointSize;
@@ -62,7 +62,7 @@ class Text extends Visual {
         return pointSize;
     }
 
-    @editable
+    @editable({ slider: [0, 10] })
     public var lineHeight(default, set):Float = 1.0;
     function set_lineHeight(lineHeight:Float):Float {
         if (this.lineHeight == lineHeight) return lineHeight;
@@ -71,7 +71,7 @@ class Text extends Visual {
         return lineHeight;
     }
 
-    @editable
+    @editable({ slider: [-100, 100] })
     public var letterSpacing(default, set):Float = 0.0;
     function set_letterSpacing(letterSpacing:Float):Float {
         if (this.letterSpacing == letterSpacing) return letterSpacing;
