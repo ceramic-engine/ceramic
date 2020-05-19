55965ecd82d65d617feba3b189b00a26e1c2ca1b
Jeremy Faivre <contact@jeremyfa.com>
2020-05-19 11:38:01 +0200

    [runtime] Fix pointInTriangle() not working in all cases

--- runtime/src/ceramic/GeometryUtils.hx
+++ runtime/src/ceramic/GeometryUtils.hx
@@ -5,9 +5,20 @@ class GeometryUtils {
 
     /** Returns `true` if the point `(x,y)` is inside the given (a,b,c) triangle */
     public static inline function pointInTriangle(x:Float, y:Float, ax:Float, ay:Float, bx:Float, by:Float, cx:Float, cy:Float):Bool {
-        return (cx - x) * (ay - y) - (ax - x) * (cy - y) >= 0 &&
-               (ax - x) * (by - y) - (bx - x) * (ay - y) >= 0 &&
-               (bx - x) * (cy - y) - (cx - x) * (by - y) >= 0;
+
+        inline function sign(x:Float, y:Float, ax:Float, ay:Float, bx:Float, by:Float):Float {
+            return (x - bx) * (ay - by) - (ax - bx) * (y - by);
+        }
+    
+        var d1:Float = sign(x, y, ax, ay, bx, by);
+        var d2:Float = sign(x, y, bx, by, cx, cy);
+        var d3:Float = sign(x, y, cx, cy, ax, ay);
+    
+        var hasNeg:Bool = (d1 < 0) || (d2 < 0) || (d3 < 0);
+        var hasPos:Bool = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    
+        return !(hasNeg && hasPos);
+
     }
 
 }
