50c56e53cedc3f2f72fdd575aa8f7757a6292afe
Jeremy Faivre <contact@jeremyfa.com>
2020-10-01 17:28:01 +0200

    [tpl] add gen/Collections

---
+++ tools/tpl/project/empty/gen/Collections.hx
@@ -0,0 +1,15 @@
+package;
+
+@:keep
+@:keepSub
+class Collections implements ceramic.AutoCollections {
+
+    public static var collections(default, null):Collections;
+
+    public function new() {
+
+        collections = this;
+
+    }
+
+}
