6eadcb4e935f468cd34bc69e2cf23a342e92f2b2
Jeremy Faivre <contact@jeremyfa.com>
2020-10-18 23:58:16 +0200

    [runtime] Add ceramic.LowRes A container to display visuals at low resolution, with the possibility to use antialiasing (supersampling)

---
+++ runtime/src/ceramic/LowRes.hx
@@ -0,0 +1,82 @@
+package ceramic;
+
+/**
+ * A container to display visuals at low resolution,
+ * with the possibility to use antialiasing (supersampling).
+ */
+class LowRes extends Layer {
+
+    /**
+     * Pixel art container to display nice and sharp pixels at any size
+     */
+    var pixelArt:PixelArt;
+
+    /**
+     * Filter used to create supersampled content
+     */
+    var filter:Filter;
+
+    /**
+     * Sharpness of the pixels (from 1.0 to above)
+     */
+    public var sharpness(get,set):Float;
+    inline function get_sharpness():Float {
+        return pixelArt.sharpness;
+    }
+    inline function set_sharpness(sharpness:Float):Float {
+        return pixelArt.sharpness = sharpness;
+    }
+
+    /**
+     * Density value used for supersampled content.
+     * A density of 1 means no supersampling (thus no antialiasing).
+     * Any value above (2 or more) will increase the supersampled content size and generate antialiasing.
+     * Use a power of two and not a too high value (2 is recommended if result is nice enough).
+     */
+    public var density(default,set):Float = 1;
+    function set_density(density:Float):Float {
+        if (this.density != density) {
+            this.density = density;
+            filter.density = density >= 1 ? density : 1;
+        }
+        return density;
+    }
+
+    /**
+     * The visual containing what should be displayed
+     */
+    public var content(default,null):Quad;
+
+    public function new() {
+
+        super();
+
+        pixelArt = new PixelArt();
+        pixelArt.density = 1;
+        add(pixelArt);
+
+        filter = new Filter();
+        filter.density = density;
+        pixelArt.content.add(filter);
+
+        content = filter.content;
+
+        onResize(this, handleResize);
+
+    }
+
+    function handleResize(width:Float, height:Float):Void {
+
+        pixelArt.size(
+            width,
+            height
+        );
+
+        filter.size(
+            width,
+            height
+        );
+
+    }
+
+}
