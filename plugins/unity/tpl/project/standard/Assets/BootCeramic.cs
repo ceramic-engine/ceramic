0463305553738bcb44e1ffbc7c7aac333d59be7e
Jeremy Faivre <contact@jeremyfa.com>
2020-11-21 01:07:56 +0100

    [unity] Add standard project template (not used yet)

---
+++ plugins/unity/tpl/project/standard/Assets/BootCeramic.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class BootCeramic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		QualitySettings.antiAliasing = 4;
+
+		haxe.root.Main.setUnityObject(this);
+		haxe.root.EntryPoint__Main.Main();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		haxe.root.Main.update();
+
+	}
+}
