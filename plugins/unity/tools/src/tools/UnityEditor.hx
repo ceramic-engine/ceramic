b602d27c16628ca7620b941d2cccf15bbc2bf02e
Jeremy Faivre <contact@jeremyfa.com>
2020-12-22 21:51:21 +0100

    [unity] Prepare for windows support of Unity backend

--- plugins/unity/tools/src/tools/UnityEditor.hx
+++ plugins/unity/tools/src/tools/UnityEditor.hx
@@ -11,13 +11,32 @@ class UnityEditor {
     public static function resolveUnityEditorPath(cwd:String, project:Project, printVersions:Bool = false):String {
 
         var unityVersion:String = null;
+        var unityEditorPath:String = null;
         if (project.app.unity != null &&
             project.app.unity.version != null) {
             unityVersion = '' + project.app.unity.version;
         }
+        if (project.app.unity != null &&
+            project.app.unity.path != null) {
+            var editorPath = '' + project.app.unity.path;
+            if (!Path.isAbsolute(editorPath)) {
+                editorPath = Path.join([cwd, editorPath]);
+            }
+            if (!FileSystem.exists(editorPath)) {
+                fail('Unity editor not found at path: $editorPath');
+            }
+            return editorPath;
+        }
 
-        if (Sys.systemName() == 'Mac') {
-            var unityEditorsPath = '/Applications/Unity/Hub/Editor/';
+        if (Sys.systemName() == 'Mac' || Sys.systemName() == 'Windows') {
+            var unityEditorsPath:String = null;
+
+            if (Sys.systemName() == 'Mac') {
+                unityEditorPath = '/Applications/Unity/Hub/Editor/';
+            }
+            else if (Sys.systemName() == 'Windows') {
+                unityEditorPath = 'C:/Program Files/Unity/Hub/Editor/';
+            }
 
             if (!FileSystem.exists(unityEditorsPath) || !FileSystem.isDirectory(unityEditorsPath)) {
                 fail('Cannot unity editor path: you need to install Unity first with Unity Hub (https://unity3d.com/get-unity/download)');
