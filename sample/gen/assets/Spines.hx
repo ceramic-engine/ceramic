517c721586ab15969dd3f73de6df3f587bc721de
Jeremy Faivre <contact@jeremyfa.com>
2021-05-01 22:33:23 +0200

    Update sample project generated files

---
+++ sample/gen/assets/Spines.hx
@@ -0,0 +1,8 @@
+package assets;
+
+@:keep @:keepSub class Spines {
+
+    public static final FX = @:privateAccess new Spines_FX();
+
+} //Spines
+
---
+++ sample/gen/assets/Spines_FX.hx
@@ -0,0 +1,68 @@
+package assets;
+
+@:keep @:keepSub class Spines_FX {
+
+    @:noCompletion private function new() {}
+
+    @:noCompletion public final _id:String = "spine:FX";
+
+    public final EXPLOSION_LEFT:String = "EXPLOSION_LEFT";
+
+    public final EXPLOSION_RIGHT:String = "EXPLOSION_RIGHT";
+
+    public final IMPACT_0:String = "IMPACT_0";
+
+    public final IMPACT_1:String = "IMPACT_1";
+
+    public final IMPACT_2:String = "IMPACT_2";
+
+    public final IMPACT_3:String = "IMPACT_3";
+
+    public final IMPACT_HAMMER:String = "IMPACT_HAMMER";
+
+    public final IMPACT_NEUTRAL:String = "IMPACT_NEUTRAL";
+
+    public final IMPACT_WRONG:String = "IMPACT_WRONG";
+
+    public final SMOKE_LEFT_0_IN:String = "SMOKE_LEFT_0_IN";
+
+    public final SMOKE_LEFT_0_LOOP:String = "SMOKE_LEFT_0_LOOP";
+
+    public final SMOKE_LEFT_1_IN:String = "SMOKE_LEFT_1_IN";
+
+    public final SMOKE_LEFT_1_LOOP:String = "SMOKE_LEFT_1_LOOP";
+
+    public final SMOKE_RIGHT_0_IN:String = "SMOKE_RIGHT_0_IN";
+
+    public final SMOKE_RIGHT_0_LOOP:String = "SMOKE_RIGHT_0_LOOP";
+
+    public final SMOKE_RIGHT_1_IN:String = "SMOKE_RIGHT_1_IN";
+
+    public final SMOKE_RIGHT_1_LOOP:String = "SMOKE_RIGHT_1_LOOP";
+
+    public final SPARK_LEFT_0:String = "SPARK_LEFT_0";
+
+    public final SPARK_LEFT_1:String = "SPARK_LEFT_1";
+
+    public final SPARK_LEFT_2:String = "SPARK_LEFT_2";
+
+    public final SPARK_LEFT_3:String = "SPARK_LEFT_3";
+
+    public final SPARK_LEFT_4:String = "SPARK_LEFT_4";
+
+    public final SPARK_LEFT_5:String = "SPARK_LEFT_5";
+
+    public final SPARK_RIGHT_0:String = "SPARK_RIGHT_0";
+
+    public final SPARK_RIGHT_1:String = "SPARK_RIGHT_1";
+
+    public final SPARK_RIGHT_2:String = "SPARK_RIGHT_2";
+
+    public final SPARK_RIGHT_3:String = "SPARK_RIGHT_3";
+
+    public final SPARK_RIGHT_4:String = "SPARK_RIGHT_4";
+
+    public final SPARK_RIGHT_6:String = "SPARK_RIGHT_6";
+
+} //Spines_FX
+
