e32fe91ac2af0af76f511fcba5eb7570778e2e0e
Jeremy Faivre <contact@jeremyfa.com>
2021-05-11 23:20:40 +0200

    [imgui] Add implicit conversion from ImTextureID and backend.TextureId

--- plugins/clay/runtime/src/backend/TextureId.hx
+++ plugins/clay/runtime/src/backend/TextureId.hx
@@ -6,4 +6,21 @@ abstract TextureId(clay.Types.TextureId) from clay.Types.TextureId to clay.Types
 
     #if (!debug && !completion) inline #end public static var DEFAULT:TextureId = Graphics.NO_TEXTURE;
 
+    #if (cpp && plugin_imgui)
+
+    @:to public inline function toImTextureID():imguicpp.ImGui.ImTextureID {
+
+        return untyped __cpp__('(void*)(long long){0}', this);
+
+    }
+
+    @:from public static inline function fromImTextureID(imTextureID:imguicpp.ImGui.ImTextureID) {
+
+        var textureId:TextureId = untyped __cpp__('(int)(long long){0}', imTextureID);
+        return textureId;
+
+    }
+
+    #end
+
 }
