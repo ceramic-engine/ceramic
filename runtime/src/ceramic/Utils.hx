39442977442731377a6945ae02465082a78ca7df
Jeremy Faivre <contact@jeremyfa.com>
2021-06-07 14:55:09 +0200

    [runtime] Add Utils.angleTo()

--- runtime/src/ceramic/Utils.hx
+++ runtime/src/ceramic/Utils.hx
@@ -499,4 +499,18 @@ class Utils {
 
     }
 
+    /**
+     * Returns the angle between (x0, y0) and (x1, y1) in degrees.
+     * @param x0 
+     * @param y0 
+     * @param x1 
+     * @param y1 
+     * @return Float
+     */
+    inline public static function angleTo(x0:Float, y0:Float, x1:Float, y1:Float):Float {
+
+        return radToDeg(Math.atan2(y1 - y0, x1 - x0)) - 90;
+
+    }
+
 }
