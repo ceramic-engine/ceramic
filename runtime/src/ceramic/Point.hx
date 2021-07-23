5ebb432218c757347b2817f87a51e1583243f7a8
Jeremy Faivre <contact@jeremyfa.com>
2021-07-23 22:54:45 +0200

    [runtime] Allow a point to be recycled

--- runtime/src/ceramic/Point.hx
+++ runtime/src/ceramic/Point.hx
@@ -20,4 +20,30 @@ class Point {
 
     }
 
+    public function recycle():Void {
+
+        this.x = 0;
+        this.y = 0;
+        pool.recycle(this);
+
+    }
+
+    public inline static function get(x:Float = 0, y:Float = 0):Point {
+
+        var point = pool.get();
+
+        if (point == null) {
+            point = new Point(x, y);
+        }
+        else {
+            point.x = x;
+            point.y = y;
+        }
+
+        return point;
+
+    }
+
+    static var pool = new Pool<Point>();
+
 }
