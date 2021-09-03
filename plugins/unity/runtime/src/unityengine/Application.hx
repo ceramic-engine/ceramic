5e1d7761928c0b495f403f36fcad48233497224f
Jeremy Faivre <contact@jeremyfa.com>
2021-09-03 17:31:07 +0200

    Add missing extern

---
+++ plugins/unity/runtime/src/unityengine/Application.hx
@@ -0,0 +1,10 @@
+package unityengine;
+
+@:native('UnityEngine.Application')
+extern class Application {
+
+    static var absoluteURL(default, null):String;
+
+    static var targetFrameRate:Int;
+
+}
