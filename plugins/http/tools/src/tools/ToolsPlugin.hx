d07410459c8d3714269352a6a0f7b973cf4fd6e5
Jeremy Faivre <contact@jeremyfa.com>
2021-10-20 17:20:45 +0200

    Make http plugin opt-in

--- plugins/http/tools/src/tools/ToolsPlugin.hx
+++ plugins/http/tools/src/tools/ToolsPlugin.hx
@@ -1,16 +1,16 @@
 package tools;
 
-import tools.Context;
-import tools.Helpers;
-import tools.Helpers.*;
-import haxe.io.Path;
 import haxe.Json;
+import haxe.io.Path;
+import tools.Context;
+import tools.Helpers.*;
+import tools.Helpers;
 
 @:keep
 class ToolsPlugin {
 
     static function main():Void {
-        
+
         var module:Dynamic = js.Node.module;
         module.exports = new ToolsPlugin();
 
@@ -31,19 +31,25 @@ class ToolsPlugin {
 
         var app = project.app;
 
-        app.paths.push(Path.join([context.plugins.get('Http').path, 'runtime/src']));
+        if (app.plugins != null && Std.isOfType(app.plugins, Array)) {
+            var plugins:Array<String> = app.plugins;
+            if (plugins.indexOf('http') != -1) {
+                // HTTP enabled
+                app.paths.push(Path.join([context.plugins.get('Http').path, 'runtime/src']));
 
-        if (context.backend != null) {
-            var availableTargets = context.backend.getBuildTargets();
-            var target = getTargetName(context.args, availableTargets);
-            var backend = context.backend.name;
+                if (context.backend != null) {
+                    var availableTargets = context.backend.getBuildTargets();
+                    var target = getTargetName(context.args, availableTargets);
+                    var backend = context.backend.name;
 
-            switch [backend, target] {
-                case [_, 'ios' | 'android' | 'windows' | 'mac' | 'linux']:
-                    // Use akifox HTTP
-                    app.libs.push('akifox-asynchttp');
-                    app.defines.akifox_asynchttp = 'dev';
-                default:
+                    switch [backend, target] {
+                        case [_, 'ios' | 'android' | 'windows' | 'mac' | 'linux']:
+                            // Use akifox HTTP
+                            app.libs.push('akifox-asynchttp');
+                            app.defines.akifox_asynchttp = 'dev';
+                        default:
+                    }
+                }
             }
         }
 
