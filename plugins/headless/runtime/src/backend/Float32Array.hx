1d33e7d7938c8eb43271983a43fac822b75e9a4b
Jeremy Faivre <contact@jeremyfa.com>
2021-11-21 22:49:31 +0100

    [headless] Add Float32Array/UInt8Array constructor with size argument

--- plugins/headless/runtime/src/backend/Float32Array.hx
+++ plugins/headless/runtime/src/backend/Float32Array.hx
@@ -1,3 +1,17 @@
 package backend;
 
-typedef Float32Array = Array<Float>;
+typedef Float32Array = Float32ArrayImplHeadless;
+
+@:forward
+abstract Float32ArrayImplHeadless(Array<Float>) from Array<Float> to Array<Float> {
+
+    public function new(size:Int) {
+
+        this = [];
+        if (size > 0) {
+            this[size-1] = 0.0;
+        }
+
+    }
+
+}
--- plugins/headless/runtime/src/backend/UInt8Array.hx
+++ plugins/headless/runtime/src/backend/UInt8Array.hx
@@ -1,3 +1,17 @@
 package backend;
 
-typedef UInt8Array = Array<Int>;
+typedef UInt8Array = UInt8ArrayImplHeadless;
+
+@:forward
+abstract UInt8ArrayImplHeadless(Array<Int>) from Array<Int> to Array<Int> {
+
+    public function new(size:Int) {
+
+        this = [];
+        if (size > 0) {
+            this[size-1] = 0;
+        }
+
+    }
+
+}
