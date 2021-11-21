6f0c856d9c3d1d701a837ef35af36761d263eb32
Jeremy Faivre <contact@jeremyfa.com>
2021-11-21 22:51:31 +0100

    [runtime] ReadOnlyPoint is now an abstract to Point

--- runtime/src/ceramic/ReadOnlyPoint.hx
+++ runtime/src/ceramic/ReadOnlyPoint.hx
@@ -2,19 +2,23 @@ package ceramic;
 
 @:structInit
 @:allow(ceramic.Screen)
-class ReadOnlyPoint {
+abstract ReadOnlyPoint(Point) from Point {
 
-    public var x(default,null):Float;
+    public var x(get,never):Float;
+    inline function get_x():Float return this.x;
 
-    public var y(default,null):Float;
+    public var y(get,never):Float;
+    inline function get_y():Float return this.y;
+
+    public inline function toPoint():Point return this;
 
 /// Print
 
     function toString():String {
 
         return '' + {
-            x: x,
-            y: y
+            x: this.x,
+            y: this.y
         };
 
     }
