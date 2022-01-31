9444711af48c0d0a1fd31c5c5647d6b00099aa6c
Jeremy Faivre <contact@jeremyfa.com>
2022-02-01 00:02:35 +0100

    quieter post-install

--- tools/post-install.js
+++ tools/post-install.js
@@ -20,9 +20,9 @@ function postInstall() {
     if (!fs.existsSync(haxelibRepoPath)) {
         fs.mkdirSync(haxelibRepoPath);
     }
-    spawnSync(haxelib, ['dev', 'generate', '../git/generate'], { stdio: "inherit", cwd: __dirname });
-    spawnSync(haxelib, ['install', 'hxcpp', '4.2.1', '--always'], { stdio: "inherit", cwd: __dirname });
-    spawnSync(haxelib, ['install', 'build.hxml', '--always'], { stdio: "inherit", cwd: __dirname });
+    spawnSync(haxelib, ['dev', 'generate', '../git/generate', '--quiet'], { stdio: "inherit", cwd: __dirname });
+    spawnSync(haxelib, ['install', 'hxcpp', '4.2.1', '--always', '--quiet'], { stdio: "inherit", cwd: __dirname });
+    spawnSync(haxelib, ['install', 'build.hxml', '--always', '--quiet'], { stdio: "inherit", cwd: __dirname });
 
     // Patch hxcpp android clang toolchain until a newer hxcpp lib is published
     var hxcppPath = (''+spawnSync(haxelib, ['path', 'hxcpp']).stdout).split("\n")[0].trim();
