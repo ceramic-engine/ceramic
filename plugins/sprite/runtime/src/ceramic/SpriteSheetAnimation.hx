c628f96a5da75017c4fc2c05e39d6369cf5d834c
Jeremy Faivre <contact@jeremyfa.com>
2023-05-18 18:25:26 +0200

    [sprite] Add `SpriteSheetAnimation.duration` (computed/observable)

--- plugins/sprite/runtime/src/ceramic/SpriteSheetAnimation.hx
+++ plugins/sprite/runtime/src/ceramic/SpriteSheetAnimation.hx
@@ -2,10 +2,27 @@ package ceramic;
 
 import tracker.Model;
 
+using ceramic.Extensions;
+
 class SpriteSheetAnimation extends Model {
 
     @serialize public var name:String = null;
 
     @serialize public var frames:ReadOnlyArray<SpriteSheetFrame> = null;
+
+    @compute public function duration():Float {
+
+        var result:Float = 0.0;
+
+        var frames = this.frames;
+        if (frames != null) {
+            for (i in 0...frames.length) {
+                result += frames.unsafeGet(i).duration;
+            }
+        }
+
+        return result;
+
+    }
 
 }
