040e9ab1e1579816daed8e2c3c8ade51ecf7a3d7
Jeremy Faivre <contact@jeremyfa.com>
2023-08-11 15:28:17 +0200

    [runtime] Support displaying an lcd-style grid when using `PixelArt` filter (need to update Unity shader too)

--- runtime/src/ceramic/PixelArt.hx
+++ runtime/src/ceramic/PixelArt.hx
@@ -20,6 +20,43 @@ class PixelArt extends Filter {
         return sharpness;
     }
 
+    /**
+     * If set above 0.0, a grid will be displayed.
+     * Can be used to simulate GBA-style LCD displays.
+     */
+    public var gridThickness(default,set):Float = 0.0;
+    function set_gridThickness(gridThickness:Float):Float {
+        if (this.gridThickness != gridThickness) {
+            this.gridThickness = gridThickness;
+            shader.setFloat('gridThickness', gridThickness);
+        }
+        return gridThickness;
+    }
+
+    /**
+     * When using a grid, this is the color of the grid
+     */
+    public var gridColor(default,set):Color = Color.BLACK;
+    function set_gridColor(gridColor:Color):Color {
+        if (this.gridColor != gridColor) {
+            this.gridColor = gridColor;
+            shader.setVec3('gridColor', gridColor.redFloat, gridColor.greenFloat, gridColor.blueFloat);
+        }
+        return gridColor;
+    }
+
+    /**
+     * When using a grid, this is the alpha of the grid
+     */
+    public var gridAlpha(default,set):Float = 0.0;
+    function set_gridAlpha(gridAlpha:Float):Float {
+        if (this.gridAlpha != gridAlpha) {
+            this.gridAlpha = gridAlpha;
+            shader.setFloat('gridAlpha', gridAlpha);
+        }
+        return gridAlpha;
+    }
+
     override function set_density(density:Float):Float {
         if (this.density == density) return density;
         super.set_density(density);
@@ -35,6 +72,9 @@ class PixelArt extends Filter {
 
         shader = ceramic.App.app.assets.shader('shader:pixelArt').clone();
         shader.setFloat('sharpness', sharpness);
+        shader.setFloat('gridThickness', 0);
+        shader.setFloat('gridAlpha', 1);
+        shader.setVec3('gridColor', 0, 0, 0);
 
         onResize(this, handleResize);
 
