7594358b7a7a6295a0e185569fbda2a053ed8de4
Jeremy Faivre <contact@jeremyfa.com>
2023-12-10 17:33:17 +0100

    [windows] One more place where we need to deal with windows build quirks

--- runtime/src/ceramic/PreloadStatus.hx
+++ runtime/src/ceramic/PreloadStatus.hx
@@ -1,5 +1,12 @@
 package ceramic;
 
+#if (cpp && windows)
+@:headerCode('
+// Needed otherwise windows build fails :(
+// But why?
+#undef ERROR
+')
+#end
 enum abstract PreloadStatus(Int) {
 
     var NONE = 0;
