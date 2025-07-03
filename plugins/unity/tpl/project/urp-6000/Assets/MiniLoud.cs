7e5f7c9cfff85545fd547668b0f1b85cacc5eb71
Jeremy Faivre <contact@jeremyfa.com>
2025-07-03 23:16:11 +0200

    [unity] Add a proper notice from original SoLoud implementation + info about the C# port "MiniLoud"

--- plugins/unity/tpl/project/urp-6000/Assets/MiniLoud.cs
+++ plugins/unity/tpl/project/urp-6000/Assets/MiniLoud.cs
@@ -3,6 +3,39 @@
 using System;
 using System.Collections.Generic;
 
+// MiniLoud is a C# port of SoLoud's mixer implementation (https://github.com/jarikomppa/soloud).
+// The port has been done by Jérémy Faivre, with the help of an LLM to facilitate the language translation.
+//
+// Original SoLoud notice:
+//
+// SoLoud contains various third party libraries which vary in licenses,
+// but are all extremely liberal; no attribution in binary form is required.
+// For more information, see SoLoud manual or http://soloud-audio.com/legal.html
+//
+// SoLoud proper is licensed under the zlib/libpng license:
+//
+// SoLoud audio engine
+// Copyright (c) 2013-2018 Jari Komppa
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//    1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+//
+//    2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+//    3. This notice may not be removed or altered from any source
+//    distribution.
+
 namespace MiniLoud
 {
     public enum Result
