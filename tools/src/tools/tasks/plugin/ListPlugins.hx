dbf7800a0189100fd2b64f397e3ef40552608b4c
Jeremy Faivre <contact@jeremyfa.com>
2026-08-06 23:17:22 +0200

    [tools] When running `ceramic plugin list`, sort plugins alphabetically

--- tools/src/tools/tasks/plugin/ListPlugins.hx
+++ tools/src/tools/tasks/plugin/ListPlugins.hx
@@ -18,8 +18,16 @@ class ListPlugins extends tools.Task {
 
     override function run(cwd:String, args:Array<String>):Void {
 
+        // Sort plugins alphabetically by displayed name (fall back to id)
+        var keys = [for (key in context.plugins.keys()) key];
+        keys.sort(function(a, b) {
+            var na = (context.plugins.get(a).name ?? a).toLowerCase();
+            var nb = (context.plugins.get(b).name ?? b).toLowerCase();
+            return na < nb ? -1 : (na > nb ? 1 : 0);
+        });
+
         // Print result
-        for (key in context.plugins.keys()) {
+        for (key in keys) {
             var info = context.plugins.get(key);
             var path:String = info.path;
             var name:String = info.name;
